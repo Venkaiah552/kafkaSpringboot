0a3c9a04002557ea712a0dbd477e1cd06effd6f5
venkaiah <venki552@gmail.com>
2019-07-27 16:25:29 +0530

    initial commit

---
+++ src/main/java/io/tpd/kafkaexample/PracticalAdvice.java
@@ -0,0 +1,26 @@
+package io.tpd.kafkaexample;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import lombok.Getter;
+
+@Getter
+public class PracticalAdvice {
+    private final String message;
+    private final int identifier;
+
+    public PracticalAdvice(@JsonProperty("message") final String message,
+                           @JsonProperty("identifier") final int identifier) {
+        this.message = message;
+        this.identifier = identifier;
+    }
+   
+
+    @Override
+    public String toString() {
+        return "PracticalAdvice::toString() {" +
+                "message='" + message + '\'' +
+                ", identifier=" + identifier +
+                '}';
+    }
+}
